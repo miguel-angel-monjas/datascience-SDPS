f3732137266441ec8d480b5001ca2c182648b0ed
miguel-angel-monjas <ma.monjas.2016@alumnos.urjc.es>
2016-11-20 19:55:47 +0100

    Tweaks

--- MapReduce/reducer_ng.py
+++ MapReduce/reducer_ng.py
@@ -2,6 +2,7 @@
 
 import sys
 
+# some nice counters
 last_state = None
 total_score = 0.0
 total_tweets = 0
@@ -9,7 +10,7 @@ total_tweets = 0
 # input comes from STDIN
 for line in sys.stdin:
     line = line.strip()
-    #print line
+
     # capture values from mapper
     state, score = line.split('\t')
     # initialization
@@ -28,5 +29,6 @@ for line in sys.stdin:
         # remaining tweets in the same state
         total_score += float(score)
         total_tweets += 1
+
 # flush final state
 print '%s;%d;%.2f' %(last_state, total_tweets, total_score/float(total_tweets))
