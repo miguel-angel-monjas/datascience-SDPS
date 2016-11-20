5b419bbf79d3eb41dff512c91ab7c031d8f32bf5
miguel-angel-monjas <ma.monjas.2016@alumnos.urjc.es>
2016-11-20 16:50:11 +0100

    First version with score

--- MapReduce/reducer_ng.py
+++ MapReduce/reducer_ng.py
@@ -6,22 +6,24 @@ import ast
 # input comes from STDIN
 
 last_state = None
-total_score = 0
+total_score = 0.0
 total_tweets = 0
 for line in sys.stdin:
     line = line.strip()
     #print line
     # capture values from mapper
     state, score = line.split('\t')
-    # initial
+    # initialization
     if last_state == None :
         last_state = state
-        total_score = score
-
+        total_score = float(score)
+        total_tweets = 1
+    # first tweet in state
     if state != last_state :
-        print '%s -> %d' %(last_state, total_score)
+        print '%s;%d;%.2f' %(last_state, total_tweets, total_score/float(total_tweets))
         last_state = state
-        total_score = score
+        total_score = float(score)
     else :
-        total_score += score
-print '%s -> %d' %(last_state, total_score)
+        total_score += float(score)
+        total_tweets += 1
+print '%s;%d;%.2f' %(last_state, total_tweets, total_score/float(total_tweets))
